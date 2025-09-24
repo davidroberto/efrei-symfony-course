fa7309b5e14fe27c0ab334a8e33dd1a8c47d9b96
David Robert <contact@david-robert.fr>
2025-09-24 11:52:32 +0200

    param id in route

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,9 +10,45 @@ class ArticleController
 
     #[Route('/articles')]
     public function renderListArticlesPage() {
-
         return new Response('<h1>Liste des articles</h1>', 200);
 
+    }
+
+    // je place dans la route un parametre variable
+    // c'est à dire qui peut être n'importe quelle valeur
+    // ici je le nomme id
+    #[Route('/articles/{id}')]
+    // je demande à symfony de récupérer automatiquement
+    // la valeur du paramete id dans l'url
+    // et de le stocker dans la variable $id
+    // donc si l'url demandée est /articles/213
+    // alors $id sera égal à 213
+    public function renderSingleArticlePage($id) {
+
+        // simulation de requête de BDD
+        $articlesFromDB = [
+            1 => [
+                'id' => 1,
+                'title' => 'Article 1',
+                'content' => 'Content Article 1',
+            ],
+            2 => [
+                'id' => 2,
+                'title' => 'Article 2',
+                'content' => 'Content Article 2',
+            ],
+            3 => [
+                'id' => 3,
+                'title' => 'Article 3',
+                'content' => 'Content Article 3',
+            ]
+        ];
+
+        // je récupère le titre de l'article dont l'id correspond à celui qui est dans l'url
+        $articleTitle = $articlesFromDB[$id]['title'];
+
+
+        return new Response("<h1> $articleTitle </h1>", 200);
     }
 
 }

e7b2440c31fccb81ab2cf81366c5f7404bad9269
David Robert <contact@david-robert.fr>
2025-09-29 08:53:49 +0200

    categorie controller

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoriesController extends AbstractController
+{
+
+    #[Route('/categories')]
+    public function renderListCategoriesPage() {
+
+        $categories = [
+            1 => [
+                'id' => 1,
+                'title' => 'Catégorie 1',
+            ],
+            2 => [
+                'id' => 2,
+                'title' => 'Catégorie 2',
+            ],
+            3 => [
+                'id' => 3,
+                'title' => 'Catégorie 3',
+            ],
+        ];
+
+
+        return $this->render('listCategories.html.twig');
+
+    }
+
+
+}
